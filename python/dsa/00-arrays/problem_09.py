2d69cdb38609600accc420faa0f6b4b06b8c5656
Waleed Shoaib <87224462+waleedshoaib2@users.noreply.github.com>
2025-12-30 02:01:09 +0500

    problem 9 solved

---
+++ python/dsa/00-arrays/problem_09.py
@@ -0,0 +1,12 @@
+def linear_search(number, array):
+    for i in range(0, len(array)):
+        if array[i] == number:
+            return i
+    else:
+        return -1
+
+array = [4, 2, 7, 1, 9]
+number = 7
+
+
+print(linear_search(number, array))
